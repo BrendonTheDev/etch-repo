45dbf614b22c0ef90cf9a5de453424f2085af367
brendon <200~108491991+BrendonTheDev@users.noreply.github.com>
2023-04-06 12:00:18 -0400

    test doc.create and append

---
+++ etchScript.js
@@ -0,0 +1,21 @@
+document.getElementsByClassName("container");
+document.createElement("div");
+
+const rows = 
+
+
+function generateRow(numRow){
+    const newDiv = document.createElement("div");
+    for (r=0; r <= numRow; r++) {
+        return newDiv;
+    }
+}
+
+function generateCells(numCells) {
+    const newCells = document.createElement("div");
+    for(c=0; c < )
+};
+
+document.body.append("container");
+document.container.append("newDiv")
+//change the second append to append output of generative functions "grid"
